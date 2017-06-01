7b9c79a46f0e20d84ae110a1de18393ae1556152
jeana <jeana@10.0.0.153>
2017-05-31 20:12:23 -0600

    JOptionPane Message Dialog

---
+++ JOptionPaneMessageDialog/src/JOptionPaneMessageDialog.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class JOptionPaneMessageDialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	JOptionPane.showMessageDialog(null, "This is a message dialog box!");
+
+	}
+
+}
